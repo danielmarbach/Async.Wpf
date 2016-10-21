9efd6e458a07339b3e066642688093761fe071eb
danielmarbach <daniel.marbach@openplace.net>
2016-10-22 00:00:42 +0200

    WPF Async Databinding sample with NitroFX and Async Databinding

---
+++ WpfNative.Tryouts/AsyncCommand.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfNative.Tryouts
+{
+    // https://msdn.microsoft.com/en-us/magazine/dn630647.aspx
+    public interface IAsyncCommand : ICommand
+    {
+        Task ExecuteAsync(object parameter);
+    }
+
+    public abstract class AsyncCommandBase : IAsyncCommand, INotifyPropertyChanged
+    {
+        public abstract bool CanExecute(object parameter);
+        public abstract Task ExecuteAsync(object parameter);
+        public async void Execute(object parameter)
+        {
+            await ExecuteAsync(parameter);
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        protected void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+    }
+
+    public class AsyncCommand<TResult> : AsyncCommandBase, INotifyPropertyChanged
+    {
+        private readonly Func<object, CancellationToken, Task<TResult>> _command;
+        private readonly CancelAsyncCommand _cancelCommand;
+        private INotifyTaskCompletion<TResult> _execution;
+        private readonly object _state;
+
+        public AsyncCommand(Func<object, CancellationToken, Task<TResult>> command, object state)
+        {
+            _state = state;
+            _command = command;
+            _cancelCommand = new CancelAsyncCommand();
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return Execution == null || Execution.IsCompleted;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            _cancelCommand.NotifyCommandStarting();
+            Execution = NotifyTaskCompletion.Create(_command(_state, _cancelCommand.Token));
+            RaiseCanExecuteChanged();
+            await Execution.TaskCompleted;
+            _cancelCommand.NotifyCommandFinished();
+            RaiseCanExecuteChanged();
+        }
+
+        public ICommand CancelCommand
+        {
+            get { return _cancelCommand; }
+        }
+
+        public INotifyTaskCompletion<TResult> Execution
+        {
+            get { return _execution; }
+            private set
+            {
+                _execution = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        sealed class CancelAsyncCommand : ICommand
+        {
+            private CancellationTokenSource _cts = new CancellationTokenSource();
+            private bool _commandExecuting;
+
+            public CancellationToken Token => _cts.Token;
+
+            public void NotifyCommandStarting()
+            {
+                _commandExecuting = true;
+                if (!_cts.IsCancellationRequested)
+                    return;
+                _cts = new CancellationTokenSource();
+                RaiseCanExecuteChanged();
+            }
+
+            public void NotifyCommandFinished()
+            {
+                _commandExecuting = false;
+                RaiseCanExecuteChanged();
+                _cts.Dispose();
+            }
+
+            bool ICommand.CanExecute(object parameter)
+            {
+                return _commandExecuting && !_cts.IsCancellationRequested;
+            }
+
+            void ICommand.Execute(object parameter)
+            {
+                _cts.Cancel();
+                RaiseCanExecuteChanged();
+            }
+
+            public event EventHandler CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
+
+            private void RaiseCanExecuteChanged()
+            {
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+    }
+
+    // extended from original to avoid closure capturing
+    public static class AsyncCommand
+    {
+        public static AsyncCommand<object> Create(Func<Task> command)
+        {
+            return new AsyncCommand<object>(async (state, _) =>
+            {
+                var cmd = (Func<Task>) state;
+                await cmd();
+                return null;
+            }, command);
+        }
+
+        public static AsyncCommand<object> Create(Func<object, Task> command, object state)
+        {
+            return new AsyncCommand<object>(async (s, _) =>
+            {
+                var cmdAndState = (Tuple<Func<object, Task>, object>)s;
+                await cmdAndState.Item1(cmdAndState.Item2);
+                return null;
+            }, Tuple.Create(command, state));
+        }
+
+        public static AsyncCommand<TResult> Create<TResult>(Func<Task<TResult>> command)
+        {
+            return new AsyncCommand<TResult>((s, _) =>
+            {
+                var cmd = (Func<Task<TResult>>)s;
+                return cmd();
+            }, command);
+        }
+
+        public static AsyncCommand<TResult> Create<TResult>(Func<object, Task<TResult>> command, object state)
+        {
+            return new AsyncCommand<TResult>((s, _) =>
+            {
+                var cmdAndState = (Tuple<Func<object, Task<TResult>>, object>)s;
+                return cmdAndState.Item1(cmdAndState.Item2);
+            }, Tuple.Create(command, state));
+        }
+
+        public static AsyncCommand<object> Create(Func<CancellationToken, Task> command)
+        {
+            return new AsyncCommand<object>(async (state, token) =>
+            {
+                var cmd = (Func<CancellationToken, Task>) state;
+                await cmd(token);
+                return null;
+            }, command);
+        }
+
+        public static AsyncCommand<object> Create(Func<object, CancellationToken, Task> command, object state)
+        {
+            return new AsyncCommand<object>(async (s, token) =>
+            {
+                var cmdAndState = (Tuple<Func<object, CancellationToken, Task>, object>)s;
+                await cmdAndState.Item1(cmdAndState.Item2, token);
+                return null;
+            }, Tuple.Create(command, state));
+        }
+
+        public static AsyncCommand<TResult> Create<TResult>(Func<CancellationToken, Task<TResult>> command)
+        {
+            return new AsyncCommand<TResult>((s, token) =>
+            {
+                var cmd = (Func<CancellationToken, Task<TResult>>)s;
+                return cmd(token);
+            }, command);
+        }
+
+        public static AsyncCommand<TResult> Create<TResult>(Func<object, CancellationToken, Task<TResult>> command, object state)
+        {
+            return new AsyncCommand<TResult>(command, state);
+        }
+    }
+}
---
+++ WpfNative.Tryouts/MainWindowViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+
+namespace WpfNative.Tryouts
+{
+    public sealed class CountUrlBytesViewModel
+    {
+        public CountUrlBytesViewModel(MainWindowViewModel parent, string url, IAsyncCommand command)
+        {
+            LoadingMessage = "Loading (" + url + ")...";
+            Command = command;
+            RemoveCommand = new DelegateCommand(() => parent.Operations.Remove(this));
+        }
+
+        public string LoadingMessage { get; private set; }
+
+        public IAsyncCommand Command { get; private set; }
+
+        public ICommand RemoveCommand { get; private set; }
+    }
+
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel()
+        {
+            Url = "http://www.example.com/";
+            Operations = new ObservableCollection<CountUrlBytesViewModel>();
+            CountUrlBytesCommand = new DelegateCommand(() =>
+            {
+                var countBytes = AsyncCommand.Create(token => MyService.DownloadAndCountBytesAsync(Url, token));
+                countBytes.Execute(null);
+                Operations.Add(new CountUrlBytesViewModel(this, Url, countBytes));
+            });
+        }
+
+        private string _url;
+        public string Url
+        {
+            get { return _url; }
+            set
+            {
+                _url = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<CountUrlBytesViewModel> Operations { get; private set; }
+
+        public ICommand CountUrlBytesCommand { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
